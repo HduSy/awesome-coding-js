99568099a79faf3bcb5f924dfa9c098615bfefc9
HduSy <hdusy14032122@gmail.com>
2020-07-27 17:07:51 +0800

    Coding in 607     93.js 复原IP 回溯法

--- ByteDance/93.js
+++ ByteDance/93.js
@@ -1,12 +1,34 @@
+function isValid(val) {
+    if (val < 0 || val > 255 || val[0] == 0 && val.length > 1)
+        return false
+    return true
+}
+
+function helper(s, res, ip) {
+    if (ip.length === 4) {
+        if (s.length === 0)
+            res.push(ip.join('.'))
+        return
+    }
+    for (let i = 1; i <= 4; i++) {
+        let val = s.substr(0, i) //make choice
+        if (!isValid(val) || i > s.length) {
+            return;
+        }
+        ip.push(val)
+        helper(s.slice(i), res, ip)
+        ip.pop()
+    }
+}
+
 /**
- * self
- * 93 复原IP地址
- * 4个整数 每个整数0-255最多三位 字符串所有字符被用上
- * ***我分析出回溯法
- * ->DFS BFS->回溯算法->
+ * 复原IP地址
+ * 回溯算法
  * @param {string} s
  * @return {string[]}
  */
 var restoreIpAddresses = function (s) {
-
+    let res = []
+    helper(s, res, [])
+    return res
 };
