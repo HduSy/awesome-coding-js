0b88c2bd56be615d2d1f49c91f29114fb42a354d
HduSy <hdusy14032122@gmail.com>
2020-09-22 21:37:43 +0800

    Coding in 607     Some Review

--- JavaScript/AScript.js
+++ JavaScript/AScript.js
@@ -1,30 +1,20 @@
-let testArr = [1, [2], [3, 4, [5]]]
-
-//Bilibili面试官周冰提供的方法
-function flat(arr) {
-    let arrStr = arr.toString()
-    return arrStr.split(',').map(Number)
+function SingleTon(name) {
+    this.name = name
+    this.instance = null
 }
 
-flat(testArr)
-
-function deepFlat(arr) {
-    let res = []
-    for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i]))
-            res = res.concat(deepFlat(arr[i]))
-        else
-            res = res.concat(arr[i])
+SingleTon.prototype.getName = function () {
+    return this.name
+}
+SingleTon.getInstance = (function () {
+    let instance = null
+    return (name) => {
+        if (!instance) {
+            instance = new SingleTon(name)
+        }
+        return instance
     }
-    return res
-}
-
-console.log(deepFlat(testArr))
-
-function reduceFlat(arr) {
-    return arr.reduce((pre, cur) => {
-        return Array.isArray(cur) ? pre.concat(reduceFlat(cur)) : pre.concat(cur)
-    }, [])
-}
-
-console.log(reduceFlat(testArr))
+})()
+let a = SingleTon.getInstance('haha')
+let b = SingleTon.getInstance('haha')
+console.log(a === b)
